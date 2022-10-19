eab2f3444f6f936e5b5f46f635cd56001ff3dc73
Metehan <metehan.bolat.ie@gmail.com>
2022-10-19 23:04:05 +0300

    Dependency Injection and Firebase constants

---
+++ app/src/main/java/com/metehanbolat/musicstreamingapp/di/Constants.kt
@@ -0,0 +1,10 @@
+package com.metehanbolat.musicstreamingapp.di
+
+object Constants {
+    const val NAME = "name"
+    const val ARTIST = "artist"
+    const val FILE_NAME = "fileName"
+    const val ALBUM_ART = "albumArt"
+    const val TRACKS = "tracks"
+    const val ALBUM_ART_ALL_CAPS = "AlbumArt"
+}
---
+++ app/src/main/java/com/metehanbolat/musicstreamingapp/di/Injection.kt
@@ -0,0 +1,11 @@
+package com.metehanbolat.musicstreamingapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Injection {
+
+}
