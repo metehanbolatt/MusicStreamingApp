4ada1e2b7e690b93c222897ef263221bf6b59362
Metehan <metehan.bolat.ie@gmail.com>
2022-10-20 00:33:32 +0300

    * Loading Screen

---
+++ app/src/main/java/com/metehanbolat/musicstreamingapp/ui/composables/LoadingScreen.kt
@@ -0,0 +1,37 @@
+package com.metehanbolat.musicstreamingapp.ui.composables
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.CircularProgressIndicator
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.metehanbolat.musicstreamingapp.R
+
+@Composable
+fun LoadingScreen() {
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.align(Alignment.Center)
+        ) {
+            Text(
+                text = stringResource(id = R.string.loadingMessage),
+                fontWeight = FontWeight.Bold,
+                fontSize = 24.sp
+            )
+            Spacer(modifier = Modifier.size(12.dp))
+            CircularProgressIndicator(
+                modifier = Modifier.padding(24.dp)
+                    .align(Alignment.CenterHorizontally)
+            )
+            
+        }
+    }
+
+}
